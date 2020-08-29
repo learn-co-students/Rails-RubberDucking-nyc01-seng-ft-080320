bb9d0049154ab62db3fefc7b6d352ba020893940
rachaelghorbani <rachael.ghorbani@gmail.com>
2020-08-29 13:26:54 -0400

    played around with some buttons

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -26,16 +26,21 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
 
-  def update
-    @student = Student.find(params[:id])
-    #byebug
-    #raise params.inspect
-    if @student.update(student_params(:name))
-        redirect_to student_path(@student)
-    else
-        render :edit
+    def update
+        @student = Student.find(params[:id])
+        #byebug
+        #raise params.inspect
+        if @student.update(student_params(:name))
+            redirect_to student_path(@student)
+        else
+            render :edit
+        end
     end
-  end
+
+    def destroy
+        Student.find(params[:id]).destroy
+        redirect_to students_path
+    end
 
   private
 
