45f6e612402799fc075a9ff787e80d60f604d17c
vincentcollis <vinsanity0050@gmail.com>
2020-08-29 16:45:16 -0400

    Done.

--- app/models/duck.rb
+++ app/models/duck.rb
@@ -1,2 +1,12 @@
 class Duck < ApplicationRecord
+    belongs_to :student, class_name: "Student", foreign_key: "student_id"
+    validates :name, presence: :true
+    validates :description, presence: :true
+    validates :student_id, numericality: {greater_than: 0, less_than: Student.all.count}
+
+    def my_owners
+        Student.all.select {|student| student.id == self.student_id}
+    end
 end
+
+
