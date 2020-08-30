d200fb48c415a5f064b1464891f5496cdabdef07
alerner1 <alyssplerner@gmail.com>
2020-08-29 22:08:40 -0400

    Done.

--- app/controllers/ducks_controller.rb
+++ app/controllers/ducks_controller.rb
@@ -1,7 +1,46 @@
 class DucksController < ApplicationController
+  before_action :find_duck, only: [:show, :edit, :update]
 
   def index
     @ducks = Duck.all
+  end
+
+  def show
+  end
+
+  def new
+    @duck = Duck.new
+  end
+
+  def create
+    @duck = Duck.new(duck_params)
+    if @duck.valid?
+      @duck.save
+      redirect_to duck_path(@duck)
+    else 
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @duck.update(duck_params)
+      redirect_to duck_path(@duck)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def duck_params
+    params.require(:duck).permit(:name, :description, :student_id)
+  end
+
+  def find_duck
+    @duck = Duck.find(params[:id])
   end
 
 end
