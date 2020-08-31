51b3d1264012be751f3deb8cacfc72de272cfef0
soroosh-gb <soroosh.gb@gmail.com>
2020-08-30 20:36:08 -0400

    Done.

--- app/controllers/ducks_controller.rb
+++ app/controllers/ducks_controller.rb
@@ -1,7 +1,52 @@
 class DucksController < ApplicationController
-
+  before_action :find_duck, only: [:show, :edit, :update]
   def index
     @ducks = Duck.all
   end
 
+  def show
+    # @duck = Duck.find(params[:id])
+  end
+
+  def new
+    @duck = Duck.new
+  end
+
+  def create
+    @duck = Duck.create(duck_params)
+    
+      if @duck.valid?
+       redirect_to duck_path(@duck)
+      else
+        flash[:my_errors] = @duck.errors.full_messages
+        redirect_to new_duck_path
+      end
+  end
+
+
+  def edit
+    # @duck = Duck.find(params[:id])
+  end
+
+  def update
+    # @duck = Duck.find(params[:id])
+    if @duck.update(duck_params)
+       redirect_to duck_path(@duck)
+    else
+      flash[:my_errors] = @duck.errors.full_messages
+      redirect_to edit_duck_path(@duck)
+    end
+  end
+
+
+  
+  private
+
+  def duck_params
+    params.require(:duck).permit!
+  end
+
+  def find_duck
+    @duck = Duck.find(params[:id])
+  end
 end
---
+++ db/migrate/20200830210144_remover_studen_id_from_ducks.rb
@@ -0,0 +1,5 @@
+class RemoverStudenIdFromDucks < ActiveRecord::Migration[5.2]
+  def change
+    remove_column :ducks, :studen_id, :integer 
+  end
+end
